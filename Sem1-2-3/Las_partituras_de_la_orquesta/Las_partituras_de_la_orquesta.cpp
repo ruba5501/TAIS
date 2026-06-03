95a1579cb9b65d3c4c8fb5e37da5dee05ea21145
Ruben Gomez <85454079+ruba5501@users.noreply.github.com>
2026-06-03 14:36:06 +0200

    Add files via upload

---
+++ Sem1-2-3/Las_partituras_de_la_orquesta/Las_partituras_de_la_orquesta.cpp
@@ -0,0 +1,93 @@
+
+/*@ <authors>
+ *
+ * Nombre, apellidos y usuario del juez (FXX) de los autores de la solución.
+ * Ruben Gomez-escalonilla f21
+ *
+ *@ </authors> */
+
+#include <iostream>
+#include <fstream>
+#include <queue>
+
+using namespace std;
+
+
+/*@ <answer>
+
+ Escribe aquí un comentario general sobre la solución, explicando cómo
+ se resuelve el problema y cuál es el coste de la solución, en función
+ del tamaño del problema.
+
+ @ </answer> */
+
+
+ // ================================================================
+ // Escribe el código completo de tu solución aquí debajo
+ // ===============================================================
+struct orquesta {
+	int musicos, partituras, carga;
+	bool operator<(orquesta const& orq2) const {
+		return carga < orq2.carga;
+	}
+};
+void sol(priority_queue<orquesta>& pq, int n, int p) {
+	int sobrantes = p - n;
+	while (sobrantes > 0) {
+		orquesta topOrq = pq.top();
+		pq.pop();
+		topOrq.partituras++;
+		topOrq.carga = (topOrq.musicos + topOrq.partituras - 1) / topOrq.partituras;
+		pq.push({ topOrq });
+		sobrantes--;
+	}
+}
+//@ <answer>
+
+bool resuelveCaso() {
+	// leer los datos de la entrada
+	int p, n;
+	cin >> p >> n;
+	if (!std::cin)  // fin de la entrada
+		return false;
+
+	orquesta orq;
+	priority_queue<orquesta> pq;
+
+	for (int i = 0; i < n; i++) {
+		cin >> orq.carga;
+		orq.musicos = orq.carga;
+		orq.partituras = 1;
+		pq.push({ orq });
+	}
+	// resolver el caso posiblemente llamando a otras funcione
+	sol(pq, n, p);
+	// escribir la solución
+	orq = pq.top();
+	cout << orq.carga << "\n";
+
+	return true;
+}
+
+//@ </answer>
+//  Lo que se escriba dejado de esta línea ya no forma parte de la solución.
+
+int main() {
+	// ajustes para que cin extraiga directamente de un fichero
+#ifndef DOMJUDGE
+	std::ifstream in("casos.txt");
+	if (!in.is_open())
+		std::cout << "Error: no se ha podido abrir el archivo de entrada." << std::endl;
+	auto cinbuf = std::cin.rdbuf(in.rdbuf());
+#endif
+
+	while (resuelveCaso());
+
+	// para dejar todo como estaba al principio
+#ifndef DOMJUDGE
+	std::cin.rdbuf(cinbuf);
+	std::cout << "Pulsa Intro para salir..." << std::flush;
+	std::cin.get();
+#endif
+	return 0;
+}
---
+++ Sem1-2-3/Pajaros_en_vuelo/Pajaros_en_vuelo.cpp
@@ -0,0 +1,83 @@
+
+/*@ <authors>
+ *
+ * Nombre, apellidos y usuario del juez (FXX) de los autores de la solución.
+ *
+ *@ </authors> */
+
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+#include "PriorityQueue.h"  // propios o los de las estructuras de datos de clase
+
+/*@ <answer>
+
+ Escribe aquí un comentario general sobre la solución, explicando cómo
+ se resuelve el problema y cuál es el coste de la solución, en función
+ del tamaño del problema.
+
+ @ </answer> */
+
+
+ // ================================================================
+ // Escribe el código completo de tu solución aquí debajo
+ // ================================================================
+ //@ <answer>
+
+void equilibrar(PriorityQueue<int, greater<int>>& max, PriorityQueue<int>& min) {
+	int d = min.size() - max.size();
+	if (d < 1) {//si max es mayor
+		int aux = max.top(); max.pop();//log(N)
+		min.push(aux);//log(N)
+	}
+	else if (d > 1) {//si min es mayor
+		int aux = min.top(); min.pop();
+		max.push(aux);//log(N)
+	}
+}
+
+bool resuelveCaso() {
+	int e, p;
+	cin >> e >> p;
+	if (e == 0 && p == 0) return false;
+	PriorityQueue<int, greater<int>> max;//cola de maximos
+	PriorityQueue<int> min;
+	min.push(e);//inserta en la cola de minimos el primer pajaro
+	for (int i = 0; i < p; i++) {//Numero de parejas,  O(P(LogN + 2LogN))
+		for (int j = 0; j < 2; j++) {//lee cada par  //coste O(2log(N))
+			cin >> e;
+			if (e > min.top())//se va construyendo los 2 monticulos
+				min.push(e);//cola minimos
+			else
+				max.push(e);//cola maximos
+		}
+		equilibrar(max, min);//O(logN)
+		cout << min.top() << " ";
+	}
+	cout << "\n";
+	return true;
+}
+
+//@ </answer>
+//  Lo que se escriba dejado de esta línea ya no forma parte de la solución.
+
+int main() {
+	// ajustes para que cin extraiga directamente de un fichero
+#ifndef DOMJUDGE
+	std::ifstream in("casos.txt");
+	if (!in.is_open())
+		std::cout << "Error: no se ha podido abrir el archivo de entrada." << std::endl;
+	auto cinbuf = std::cin.rdbuf(in.rdbuf());
+#endif
+
+	while (resuelveCaso());
+
+	// para dejar todo como estaba al principio
+#ifndef DOMJUDGE
+	std::cin.rdbuf(cinbuf);
+	std::cout << "Pulsa Intro para salir..." << std::flush;
+	std::cin.get();
+#endif
+	return 0;
+}
