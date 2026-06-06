56c57e127d137e254540969ae321aab8beec46da
Ruben Gomez <85454079+ruba5501@users.noreply.github.com>
2026-06-06 14:30:48 +0200

    Add files via upload

---
+++ Sem4-5-6(grafos)/Los_amigos_de_mis_amigos_son_mis_amigos/Los_amigos_de_mis_amigos_son_mis_amigos.cpp
@@ -0,0 +1,118 @@
+/*@ <authors>
+ *
+ * Nombre, apellidos y usuario del juez (FXX) de los autores de la solución.
+ *
+ *@ </authors> */
+
+#include <iostream>
+#include <fstream>
+#include <queue>
+
+using namespace std;
+
+#include "Grafo.h"  // propios o los de las estructuras de datos de clase
+
+/*@ <answer>
+
+ Escribe aquí un comentario general sobre la solución, explicando cómo
+ se resuelve el problema y cuál es el coste de la solución, en función
+ del tamaño del problema.
+
+ @ </answer> */
+
+class CaminosDFS {
+private:
+	std::vector<bool> visit; // visit[v] = ¿hay camino de s a v?
+	std::vector<int> ant; // ant[v] = último vértice antes de llegar a v
+	int cont;
+	void dfs(Grafo const& G, int v) {
+		visit[v] = true;
+		for (int w : G.ady(v)) {
+			if (!visit[w]) {
+				cont++;
+				ant[w] = v;
+				dfs(G, w);
+			}
+		}
+	}
+public:
+	CaminosDFS(Grafo const& g) : visit(g.V(), false),
+		ant(g.V()), cont(1) {
+		for (int i = 1; i < g.V(); i++) {
+			if (!visit[i]) {
+				dfs(g, i);
+				if (cont > contMax) contMax = cont;
+				cont = 1;
+			}
+		}
+	}
+	int contMax = 0;
+	// ¿hay camino del origen a v?
+	bool hayCamino(int v) const {
+		return visit[v];
+	}
+
+	int grupoMasGrande() {
+		return contMax;
+	}
+	/*using Camino = std::deque<int>; // para representar caminos
+	// devuelve un camino desde el origen a v (debe existir)
+	Camino camino(int v) const {
+		if (!hayCamino(v))
+			throw std::domain_error("No existe camino");
+		Camino cam;
+		// recuperamos el camino retrocediendo
+		for (int x = v; x != s; x = ant[x])
+			cam.push_front(x);
+		cam.push_front(s);
+		return cam;
+	}*/
+};
+
+ // ================================================================
+ // Escribe el código completo de tu solución aquí debajo
+ // ================================================================
+ //@ <answer>
+
+void resuelveCaso() {
+	// leer los datos de la entrada
+	int n, m;
+	cin >> n >> m;
+	Grafo g(n+1);
+	int a1, a2;
+	for (int i = 0; i < m; i++) {
+		cin >> a1 >> a2;
+		g.ponArista(a1, a2);
+	}
+	// resolver el caso posiblemente llamando a otras funciones
+	CaminosDFS dfs(g);
+	// escribir la solución
+	cout << dfs.grupoMasGrande() << "\n";
+}
+
+//@ </answer>
+//  Lo que se escriba dejado de esta línea ya no forma parte de la solución.
+
+int main() {
+	// ajustes para que cin extraiga directamente de un fichero
+#ifndef DOMJUDGE
+	std::ifstream in("casos.txt");
+	if (!in.is_open())
+		std::cout << "Error: no se ha podido abrir el archivo de entrada." << std::endl;
+	auto cinbuf = std::cin.rdbuf(in.rdbuf());
+#endif
+
+	int numCasos;
+	std::cin >> numCasos;
+	for (int i = 0; i < numCasos; ++i)
+		resuelveCaso();
+
+	// para dejar todo como estaba al principio y parar antes de salir
+#ifndef DOMJUDGE
+	std::cin.rdbuf(cinbuf);
+	std::cout << "Pulsa Intro para salir..." << std::flush;
+	std::cin.get();
+#endif
+
+	return 0;
+}
---
+++ Sem4-5-6(grafos)/Peaje_a_la_sombra/Peaje_a_la_sombra.cpp
@@ -0,0 +1,123 @@
+
+/*@ <authors>
+ *
+ * Nombre, apellidos y usuario del juez (FXX) de los autores de la solución.
+ *
+ *@ </authors> */
+
+#include <iostream>
+#include <fstream>
+#include <queue>
+
+using namespace std;
+
+#include "Grafo.h"  // propios o los de las estructuras de datos de clase
+
+/*@ <answer>
+
+ Escribe aquí un comentario general sobre la solución, explicando cómo
+ se resuelve el problema y cuál es el coste de la solución, en función
+ del tamaño del problema.
+
+ @ </answer> */
+
+class CaminoMasCorto {
+public:
+	CaminoMasCorto(Grafo const& g, int s) : visit(g.V(), false),
+		ant(g.V()), dist(g.V()), s(s) {
+		bfs(g);
+	}
+	// ¿hay camino del origen a v?
+	bool haCamino(int v) const {
+		return visit[v];
+	}
+	// número de aristas entre s y v
+	int distancia(int v) const {
+		return dist[v];
+	}
+	/*// devuelve el camino más corto desde el origen a v (si existe)
+	Camino camino(int v) const {
+		if (!hayCamino(v)) throw std::domain_error("No existe camino");
+		Camino cam;
+		for (int x = v; x != s; x = ant[x])
+			cam.push_front(x);
+		cam.push_front(s);
+		return cam;
+	}*/
+private:
+	std::vector<bool> visit; // visit[v] = ¿hay camino de s a v?
+	std::vector<int> ant; // ant[v] = último vértice antes de llegar a v
+	std::vector<int> dist; // dist[v] = aristas en el camino s-v más corto
+	int s;
+	void bfs(Grafo const& g) {
+		std::queue<int> q;
+		dist[s] = 0; visit[s] = true;
+		q.push(s);
+		while (!q.empty()) {
+			int v = q.front(); q.pop();
+			for (int w : g.ady(v)) {
+				if (!visit[w]) {
+					ant[w] = v; dist[w] = dist[v] + 1; visit[w] = true;
+					q.push(w);
+				}
+			}
+		}
+	}
+};
+
+ // ================================================================
+ // Escribe el código completo de tu solución aquí debajo
+ // ================================================================
+ //@ <answer>
+
+bool resuelveCaso() {
+	// leer los datos de la entrada
+	int n, m, alex, lucas, trabajo;
+	cin >> n >> m >> alex >> lucas >> trabajo;
+	if (!std::cin)  // fin de la entrada
+		return false;
+
+	// resolver el caso posiblemente llamando a otras funciones
+	Grafo g(n+1);
+	int a1, a2;
+	for (int i = 0; i < m; i++) {
+		cin >> a1 >> a2;
+		g.ponArista(a1, a2);
+	}
+	// escribir la solución
+	CaminoMasCorto caminoAlex(g, alex);
+	CaminoMasCorto caminoLucas(g, lucas);
+	CaminoMasCorto caminoTrabajo(g, trabajo);
+	int coste = 99999999999;
+	for (int i = 1; i < g.V(); i++) {
+		int aux = caminoAlex.distancia(i) + caminoLucas.distancia(i) + caminoTrabajo.distancia(i);
+		if (aux < coste) coste = aux;
+	}
+
+	cout << coste << "\n";
+
+	return true;
+}
+
+//@ </answer>
+//  Lo que se escriba dejado de esta línea ya no forma parte de la solución.
+
+int main() {
+	// ajustes para que cin extraiga directamente de un fichero
+#ifndef DOMJUDGE
+	std::ifstream in("casos.txt");
+	if (!in.is_open())
+		std::cout << "Error: no se ha podido abrir el archivo de entrada." << std::endl;
+	auto cinbuf = std::cin.rdbuf(in.rdbuf());
+#endif
+
+	while (resuelveCaso());
+
+	// para dejar todo como estaba al principio
+#ifndef DOMJUDGE
+	std::cin.rdbuf(cinbuf);
+	std::cout << "Pulsa Intro para salir..." << std::flush;
+	std::cin.get();
+#endif
+	return 0;
+}
